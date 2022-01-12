a40a8bac6afb0bcb7d9930bb378a9d077e6337ae
Paul Peregud <paulperegud@protonmail.ch>
2022-01-12 13:48:47 +0100

    use to_public instead of EC math
    * update comments
    * move test dependencies to [dev-dependencies]

--- wildland-crypto/src/identity/mod.rs
+++ wildland-crypto/src/identity/mod.rs
@@ -1,32 +1,29 @@
 //
 // Wildland Project
-// 
+//
 // Copyright © 2021 Golem Foundation,
+// 	    	     Pawel Peregud <pepesza@wildland.io>
 // 	    	     Piotr K. Isajew <piotr@wildland.io>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use std::fmt;
 
 use bip39::{Language, Mnemonic};
-use cryptoxide::curve25519::ge_scalarmult_base;
-// for signing and signature verification
-use cryptoxide::ed25519;
-use cryptoxide::ed25519::{SIGNATURE_LENGTH, to_public};
+use cryptoxide::ed25519::to_public;
 use ed25519_bip32::{DerivationScheme, XPrv};
 use hex::encode;
-use hex_literal::hex;
 use hkdf::Hkdf;
 use sha2::Sha256;
 
@@ -58,7 +55,6 @@ pub struct Identity {
 }
 
 pub fn from_random_seed() -> Box<Identity> {
-    // rust-bip39
     let mnemonic = Mnemonic::generate(12).unwrap();
     let mut vec: Vec<String> = Vec::new();
     for word in mnemonic.word_iter() {
@@ -66,7 +62,6 @@ pub fn from_random_seed() -> Box<Identity> {
     }
     from_mnemonic(&vec).unwrap()
 }
-
 
 pub fn from_mnemonic(phrase: &Vec<String>) -> Result<Box<Identity>, CargoError> {
     if phrase.len() != 12 {
@@ -97,16 +92,20 @@ pub fn from_mnemonic(phrase: &Vec<String>) -> Result<Box<Identity>, CargoError> 
             for (i, word) in phrase.iter().enumerate() {
                 words[i] = word.to_string();
             }
-            Ok(Box::new(Identity { xprv: root_xprv, words: words }))
+            Ok(Box::new(Identity {
+                xprv: root_xprv,
+                words: words,
+            }))
         }
     }
 }
 
 fn extend_seed(seed: [u8; 64], target: &mut [u8; 96]) {
     let initial_key_material = seed;
-    let info = hex!("57696c646c616e64"); // b'Wildland'.hex()
+    let info = [87, 105, 108, 100, 108, 97, 110, 100]; // list(b'Wildland')
     let hk = Hkdf::<Sha256>::new(None, &initial_key_material);
-    hk.expand(&info, target).expect("Should return 96 bytes of randomness");
+    hk.expand(&info, target)
+        .expect("Should return 96 bytes of randomness");
 }
 
 impl Identity {
@@ -146,13 +145,14 @@ impl Identity {
             secret_xprv = (&secret_xprv).derive(DerivationScheme::V2, di);
         }
 
-        // drop last 32 bytes of chain-code from xprv to get secret key
+        // drop the chain-code from xprv to get secret key
         let secret: Vec<u8> = secret_xprv.as_ref()[..64].to_vec();
-        // drop chain-code AND last 32 bytes of secret key to get public key
-        let public = ge_scalarmult_base(&secret_xprv.as_ref()[0..32])
-            .to_bytes()
-            .to_vec();
-        Box::new(KeyPair { pubkey: public, seckey: secret })
+        // drop the chain-code from xprv and generate public key from the secret key
+        let public = to_public(&secret_xprv.as_ref()[0..64]).to_vec();
+        Box::new(KeyPair {
+            pubkey: public,
+            seckey: secret,
+        })
     }
 }
 
@@ -170,7 +170,6 @@ fn single_use_encryption_key_path(index: u64) -> String {
     format!("/m/574c44/2/{}", index)
 }
 
-
 pub struct KeyPair {
     pubkey: Vec<u8>,
     seckey: Vec<u8>,
@@ -187,18 +186,21 @@ impl KeyPair {
 }
 
 #[cfg(test)]
+
 mod tests {
     use super::*;
+    use cryptoxide::ed25519;
+    use cryptoxide::ed25519::SIGNATURE_LENGTH;
+    use hex_literal::hex;
 
     const MSG: &'static [u8] = b"Hello World";
 
     fn user() -> Box<Identity> {
         let mnemonic_string = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
-        let mnemonic_vec: Vec<String> =
-            mnemonic_string
-                .split(" ")
-                .map(|s| s.to_string())
-                .collect::<Vec<String>>();
+        let mnemonic_vec: Vec<String> = mnemonic_string
+            .split(" ")
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
         from_mnemonic(&mnemonic_vec).unwrap()
     }
 
@@ -244,37 +246,42 @@ mod tests {
     #[test]
     fn expanding_the_seed() {
         // vectors constructed using 'hkdf' python package
-        let ikm = hex!("
+        let ikm = hex!(
+            "
             0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b
             0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b
             0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b
             0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b
-        ");
+        "
+        );
         let mut output_key_material = [0u8; 96];
         extend_seed(ikm, &mut output_key_material);
         let (secret_key, chain_code) = output_key_material.split_at(64);
-        let expected_secret = hex!("
+        let expected_secret = hex!(
+            "
             540d175899e60c3fae2e80592a19ef98
             3b26186b5b4be4bbb9cf590ab401d689
             7e293e76ac281196ec04b7bc68d2e8a0
             36ef6b6171f6fcde3836fdaacbd1a661
-        ");
+        "
+        );
         assert_eq!(secret_key, expected_secret);
-        let expected_chain_code = hex!("
+        let expected_chain_code = hex!(
+            "
             d4d1716dc1a50023fc97267109d4e4e7
             b1ff0ba00e5404d7127b48bfd4900e79
-        ");
+        "
+        );
         assert_eq!(chain_code, expected_chain_code);
     }
 
     #[test]
     fn can_recover_seed() {
         let mnemonic_string = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
-        let mnemonic_vec: Vec<String> =
-            mnemonic_string
-                .split(" ")
-                .map(|s| s.to_string())
-                .collect::<Vec<String>>();
+        let mnemonic_vec: Vec<String> = mnemonic_string
+            .split(" ")
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
         let user = from_mnemonic(&mnemonic_vec).unwrap();
         assert_eq!(user.mnemonic().len(), 12);
     }
