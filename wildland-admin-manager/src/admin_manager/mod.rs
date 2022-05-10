0b9ecbabbe240f04b015c6c57f871de36b4be25d
szymon.baginski <baginski.szymon@gmail.com>
2022-05-10 14:03:38 +0000

    add seed phrase to api

---
+++ wildland-admin-manager/src/admin_manager/mod.rs
@@ -0,0 +1,44 @@
+use crate::api;
+use bip39::{Language, Mnemonic};
+
+#[derive(Default, Clone)]
+struct SeedPhrase(Vec<String>);
+
+impl api::SeedPhrase for SeedPhrase {
+    fn set_words(&mut self, words: Vec<String>) {
+        self.0 = words
+    }
+
+    fn get_words(&self) -> Vec<String> {
+        self.0.clone()
+    }
+}
+
+#[derive(Default)]
+pub struct AdminManager<S: api::SeedPhrase> {
+    seed_phrase: S,
+}
+
+impl api::AdminManager<SeedPhrase> for AdminManager<SeedPhrase> {
+    fn generate_seed_phrase(&mut self) -> SeedPhrase {
+        self.seed_phrase.0 = Mnemonic::generate_in(Language::English, 12)
+            .unwrap()
+            .word_iter()
+            .map(|s| s.into())
+            .collect();
+        self.seed_phrase.clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::AdminManager;
+    use crate::api::{AdminManager as AdminManagerApi, SeedPhrase};
+
+    #[test]
+    fn test_seed_phrase_len() {
+        let mut admin_manager = AdminManager::default();
+        let seed_phrase = admin_manager.generate_seed_phrase();
+        assert_eq!(seed_phrase.get_words().len(), 12);
+    }
+}
--- wildland-admin-manager/src/lib.rs
+++ wildland-admin-manager/src/lib.rs
@@ -1,3 +1,6 @@
+pub mod admin_manager;
+pub mod api;
+
 pub fn get_version() -> &'static str {
     env!("CARGO_PKG_VERSION")
 }
