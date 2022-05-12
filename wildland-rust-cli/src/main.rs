9caa66da0251430e9fe88e2a672809a30d0d6129
Szymon Bagiński <szymon@wildland.io>
2022-05-12 12:54:08 +0200

    refactor

--- wildland-rust-cli/src/main.rs
+++ wildland-rust-cli/src/main.rs
@@ -1,71 +1,18 @@
-use clap::{AppSettings, Parser};
+use clap::StructOpt;
+use cli_args::{CliArgs, IdentitySubCommand, SubCommand};
 use wildland_admin_manager::{
     admin_manager::{AdminManager, Identity},
     api::AdminManager as ApiAdminManager,
 };
-use yansi::Paint;
 
-fn get_version() -> &'static str {
-    env!("CARGO_PKG_VERSION")
-}
-
-fn print_version() {
-    println!(
-        "[{}] Wildland CLI version {}",
-        Paint::green("+").bold(),
-        get_version()
-    );
-    println!(
-        "[{}] Admin Manager version {}",
-        Paint::green("+").bold(),
-        wildland_admin_manager::get_version(),
-    );
-
-    println!("[{}] Core library version:", Paint::blue("+").bold());
-    wildland_corex::get_version_verbose()
-        .iter()
-        .for_each(|(name, version)| {
-            println!(
-                "[{}] * {} version {}",
-                Paint::blue("+").bold(),
-                name,
-                version
-            )
-        });
-}
-
-#[derive(clap::Subcommand)]
-enum IdentitySubCommand {
-    Generate,
-    Restore { seed_phrase: String },
-}
-
-#[derive(clap::Subcommand)]
-enum SubCommand {
-    Identity {
-        #[clap(subcommand)]
-        identity_action: IdentitySubCommand,
-    },
-}
-
-#[derive(Parser)]
-#[clap(
-    about,
-    arg_required_else_help = true,
-    global_setting(AppSettings::NoAutoVersion)
-)]
-struct CliArgs {
-    #[clap(long, short = 'V')]
-    version: bool,
-    #[clap(subcommand)]
-    sub_command_action: SubCommand,
-}
+mod cli_args;
+mod version;
 
 fn main() {
     let cli = CliArgs::parse();
 
     if cli.version {
-        print_version();
+        version::print_version();
     } else {
         let mut admin_manager = AdminManager::<Identity>::default();
         match cli.sub_command_action {
