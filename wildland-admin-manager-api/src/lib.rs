46f2e7fe4b43917864d502418cb43d1b6dbe2730
Szymon Bagiński <szymon@wildland.io>
2022-05-17 12:34:06 +0200

    remove seed phrase file

--- wildland-admin-manager-api/src/lib.rs
+++ wildland-admin-manager-api/src/lib.rs
@@ -1,11 +1,12 @@
 mod admin_manager;
 mod error;
 mod identity;
-mod seed_phrase;
 
 pub use admin_manager::AdminManager;
 pub use error::*;
 pub use identity::{Identity, IdentityType};
-pub use seed_phrase::{SeedPhraseWords, SEED_PHRASE_LEN};
 
 pub type AdminManagerResult<T> = std::result::Result<T, AdminManagerError>;
+
+pub const SEED_PHRASE_LEN: usize = 12;
+pub type SeedPhraseWords = [String; SEED_PHRASE_LEN];
--- wildland-admin-manager-api/src/seed_phrase.rs
+++
@@ -1,2 +0,0 @@
-pub const SEED_PHRASE_LEN: usize = 12;
-pub type SeedPhraseWords = [String; SEED_PHRASE_LEN];
