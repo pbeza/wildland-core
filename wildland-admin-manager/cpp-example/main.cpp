23eafe458cea103fb52310fa198124da204f830d
Szymon Bagiński <szymon@wildland.io>
2022-05-24 09:21:10 +0200

    add cpp example

---
+++ wildland-admin-manager/cpp-example/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "../../target/cxxbridge/wildland-admin-manager/src/ffi/mod.rs.h"
+
+using namespace cargo::api;
+
+int main()
+{
+    auto am = create_admin_manager();
+
+    auto seed_result = create_seed_phrase();
+    if (seed_result->is_ok())
+    {
+        auto &seed_ok_ref = seed_result->unwrap();
+        auto seed_str = std::string(seed_ok_ref.get_string());
+        std::cout << "Generated seed: " << seed_str << std::endl;
+
+        auto identity_result = am->create_master_identity_from_seed_phrase(::rust::String{"Some generic name"}, seed_ok_ref);
+
+        auto optional_identity = am->get_master_identity(); // The same identity as inside the result above
+        if (optional_identity->is_some())
+        {
+            auto &identity = optional_identity->unwrap();
+
+            std::cout << "Identity name: " << std::string(identity.get_name()) << std::endl;
+            identity.set_name(::rust::String{"New name"});
+            std::cout << "Identity name: " << std::string(identity.get_name()) << std::endl;
+        }
+    }
+    else
+    {
+        auto &seed_err_ref = seed_result->unwrap_err();
+        auto err_msg = std::string(seed_err_ref.to_string());
+        auto code = seed_err_ref.code();
+        std::cout << code << std::endl;
+    }
+}
