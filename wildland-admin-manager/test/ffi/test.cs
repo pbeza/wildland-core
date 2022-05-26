cb9257574ae382b1fa161a685297954ae6cb17c1
Szymon Bagiński <szymon@wildland.io>
2022-05-26 15:36:43 +0200

    csharp test

--- wildland-admin-manager/test/ffi/test.cs
+++ wildland-admin-manager/test/ffi/test.cs
@@ -7,6 +7,17 @@ namespace Main
         static void Main(string[] args)
         {
             Console.WriteLine("TODO: C# FFI Testsuite");
+        
+            var admin_manager = wildland.create_admin_manager();
+            var seed_result = wildland.create_seed_phrase();
+            if (seed_result.is_ok()) {
+                var seed = seed_result.unwrap();
+                var identity_result = admin_manager.create_master_identity_from_seed_phrase(new RustString("name 1"), seed);
+                var identity = identity_result.unwrap();
+                Console.WriteLine(identity.get_name().c_str());
+                identity.set_name(new RustString("name 2"));
+                Console.WriteLine(identity.get_name().c_str());
+            }
         }
     }
 }
