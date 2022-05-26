9f276a07945991e3d4e2e07ceec1fc2cc3714067
Szymon Bagiński <szymon@wildland.io>
2022-05-26 15:43:38 +0200

    python test

--- wildland-admin-manager/test/ffi/test.py
+++ wildland-admin-manager/test/ffi/test.py
@@ -7,3 +7,13 @@ print("TODO: Python FFI Testsuite")
 import ctypes
 def deref_ptr(ptr, typ):
     return ctypes.cast(int(ptr), ctypes.POINTER(typ))[0]
+
+admin_manager = wildland.create_admin_manager()
+seed_result = wildland.create_seed_phrase()
+if seed_result.is_ok():
+    seed = seed_result.unwrap()
+    identity_result = admin_manager.create_master_identity_from_seed_phrase(wildland.RustString("name 1"), seed)
+    identity = identity_result.unwrap()
+    print(identity.get_name().c_str())
+    identity.set_name(wildland.RustString("name 2"))
+    print(identity.get_name().c_str())
