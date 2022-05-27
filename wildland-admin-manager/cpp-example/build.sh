3a1836477a01842b8fab2724e8cb3a68df7767f3
Szymon Bagiński <szymon@wildland.io>
2022-05-27 15:25:49 +0200

    remove cpp example

--- wildland-admin-manager/cpp-example/build.sh
+++
@@ -1,5 +0,0 @@
-g++ -std=c++20 -o out main.cpp \
-../../target/debug/build/cxx-e81bc35d61dcff50/out/src/cxx.o \
-../../target/cxxbridge/wildland-admin-manager/src/ffi/mod.rs.cc \
--I ../../target/cxxbridge/ \
--L ../../target/debug -lwildland_admin_manager
