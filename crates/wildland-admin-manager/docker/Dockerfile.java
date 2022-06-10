e2dd16d82ba821d34a23ace434b3cdd2c8ddba2e
Szymon Bagiński <szymon@wildland.io>
2022-06-10 14:00:27 +0200

    better docker layers caching

--- crates/wildland-admin-manager/docker/Dockerfile.java
+++ crates/wildland-admin-manager/docker/Dockerfile.java
@@ -1,27 +1,41 @@
-FROM wildland-sdk-base:latest
+FROM debian:testing-slim
 
 # Run with:
 #
 # docker-compose -f wildland-admin-manager/docker/docker-compose.yml run --rm wildland-sdk-java
 
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get -qy update && apt-get install -y swig openjdk-17-jdk-headless g++
+
+ENV WLTMP=/wildland-core/crates/wildland-admin-manager/_temporary/
+ENV TARGET=/wildland-core/target/
 ENV CC=g++
 ENV JDK_INC_DIR=/usr/lib/jvm/java-17-openjdk-amd64/include
 
-RUN apt-get -qy update && apt-get install -y swig openjdk-17-jdk
+RUN mkdir -p /wildland-core/
+WORKDIR /wildland-core
+
+# Copy from base image instead of building new image on top of it to avoid reinstalling packages after source code changes
+COPY --from=wildland-sdk-base ${WLTMP} ./cpp/
+# TODO copy only libs
+COPY --from=wildland-sdk-base ${TARGET} ./lib/
+COPY crates/wildland-admin-manager/wildland.i ./
+COPY crates/wildland-admin-manager/test/ffi/test.java ./
 
 RUN mkdir -p wildland_java \
-    && cd "${WLTMP}" \
+    && cd cpp \
     && swig -java -c++ -w516,503,476,302 -outdir ../wildland_java ../wildland.i \
     && mv ../wildland_wrap.cxx . \
     && ${CC} -fpermissive -shared -fPIC --std=c++14 -w \
     wildland_wrap.cxx mod.rs.cc \
-    -L../../../target/debug \
+    -L ../lib/debug \
     -lwildland_admin_manager \
     -I${JDK_INC_DIR} \
     -I${JDK_INC_DIR}/linux \
     -o ../wildland_java/libwildland.so
 
-RUN cp test/ffi/test.java wildland_java/test.java \
+RUN cp test.java wildland_java/test.java \
     && cd wildland_java \
     && javac test.java
 
