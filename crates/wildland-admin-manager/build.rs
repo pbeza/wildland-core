5cbd724d2f6ddabefd4f685b1abb0aa82653127a
Tomasz Kulik <tomasz@wildland.io>
2022-07-07 10:39:36 +0200

    Add env variable to point the target dirs for cpp and swift generated code

--- crates/wildland-admin-manager/build.rs
+++ crates/wildland-admin-manager/build.rs
@@ -1,7 +1,11 @@
 #[cfg(feature = "bindings")]
 fn main() {
     use ffi_macro_build::parse_ffi_module;
-    parse_ffi_module("src/ffi/mod.rs", "./_generated_swift/", "./_generated_cpp/");
+    let swift_target_dir =
+        std::env::var("SWIFT_TARGET_DIR").unwrap_or_else(|_| "./_generated_swift/".to_owned());
+    let cpp_target_dir =
+        std::env::var("CPP_TARGET_DIR").unwrap_or_else(|_| "./_generated_cpp/".to_owned());
+    parse_ffi_module("src/ffi/mod.rs", &swift_target_dir, &cpp_target_dir);
     println!("cargo:rerun-if-changed=src/ffi/mod.rs");
 }
 
