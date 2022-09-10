7e218add5d2a168a8815952c7a454a0465164896
Łukasz Kujawski <leon@wildland.io>
2022-09-10 19:13:10 +0200

    WILX-159: formatting

--- crates/wildland-http-client/src/evs/client.rs
+++ crates/wildland-http-client/src/evs/client.rs
@@ -22,35 +22,38 @@ pub struct GetStorageRes {
 
 #[derive(Clone, Default, Debug)]
 pub struct EVSClient {
-    pub(crate) base_url: String,
-    pub(crate) client: Client,
+    _base_url: String,
+    _client: Client,
 }
 
 impl EVSClient {
     #[tracing::instrument(level = "debug", ret)]
-    pub fn new(base_url: &str) -> Self {
+    pub fn _new(base_url: &str) -> Self {
         let client = Client::new();
         Self {
-            base_url: base_url.to_string(),
-            client,
+            _base_url: base_url.to_string(),
+            _client: client,
         }
     }
 
     #[tracing::instrument(level = "debug", ret, skip(self))]
-    pub async fn confirm_token(
+    pub async fn _confirm_token(
         &self,
         request: ConfirmTokenReq,
     ) -> Result<(), WildlandHttpClientError> {
-        let url = format!("{}/confirm_token", self.base_url);
-        let response = self.client.put(url).json(&request).send().await?;
+        let url = format!("{}/confirm_token", self._base_url);
+        let response = self._client.put(url).json(&request).send().await?;
         handle(response).await?;
         Ok(())
     }
 
     #[tracing::instrument(level = "debug", ret, skip(self))]
-    pub async fn get_storage(&self, request: GetStorageReq) -> Result<GetStorageRes, WildlandHttpClientError> {
-        let url = format!("{}/get_storage", self.base_url);
-        let response = self.client.put(url).json(&request).send().await?;
+    pub async fn _get_storage(
+        &self,
+        request: GetStorageReq,
+    ) -> Result<GetStorageRes, WildlandHttpClientError> {
+        let url = format!("{}/get_storage", self._base_url);
+        let response = self._client.put(url).json(&request).send().await?;
         let response_json = handle(response).await?.json().await?;
         Ok(response_json)
     }
@@ -58,7 +61,9 @@ impl EVSClient {
 
 #[cfg(test)]
 mod tests {
-    use crate::evs::constants::test_utilities::{EMAIL, ENCRYPTED_CREDENTIALS, PUBKEY, VERIFICATION_TOKEN};
+    use crate::evs::constants::test_utilities::{
+        EMAIL, ENCRYPTED_CREDENTIALS, PUBKEY, VERIFICATION_TOKEN,
+    };
     use mockito::{mock, server_url};
     use serde_json::json;
 
@@ -66,8 +71,8 @@ mod tests {
 
     fn client() -> EVSClient {
         EVSClient {
-            base_url: server_url(),
-            client: Client::new(),
+            _base_url: server_url(),
+            _client: Client::new(),
         }
     }
 
@@ -79,11 +84,10 @@ mod tests {
             verification_token: VERIFICATION_TOKEN.into(),
         };
 
-        let m = mock("PUT", "/confirm_token")
-            .create();
+        let m = mock("PUT", "/confirm_token").create();
 
         // when
-        let response = client().confirm_token(request).await;
+        let response = client()._confirm_token(request).await;
 
         // then
         m.assert();
@@ -99,16 +103,11 @@ mod tests {
         };
 
         let m = mock("PUT", "/get_storage")
-            .with_body(
-                json!({ "encrypted_credentials": ENCRYPTED_CREDENTIALS }).to_string(),
-            )
+            .with_body(json!({ "encrypted_credentials": ENCRYPTED_CREDENTIALS }).to_string())
             .create();
 
         // when
-        let response = client()
-            .get_storage(request)
-            .await
-            .unwrap();
+        let response = client()._get_storage(request).await.unwrap();
 
         // then
         m.assert();
