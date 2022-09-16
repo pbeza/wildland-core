1f0d7f38f2a6c3afa095d9e433890dcb27514a59
Szymon Bagiński <szymon@wildland.io>
2022-09-16 16:17:06 +0200

    add create_cargo_lib doc string

--- crates/wildland-cargo-lib/src/lib.rs
+++ crates/wildland-cargo-lib/src/lib.rs
@@ -9,6 +9,12 @@ pub use api::user::{MnemonicPayload, UserApi, UserPayload};
 pub use cargo_lib::CargoLib;
 use wildland_corex::{LocalSecureStorage, LssService, UserService};
 
+/// Function creating [`CargoLib`] structure which is the main part of Cargo public API.
+/// All functionalities are exposed to application side through this structure.
+///
+/// **Arguments**:
+/// - lss: object implementing [`LocalSecureStorage`] trait
+///
 pub fn create_cargo_lib(lss: &'static dyn LocalSecureStorage) -> CargoLib {
     // TODO WILX-219 Memory leak
     _ = logging::init_subscriber();
