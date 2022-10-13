bb965796266df14efae379e956c76f6e9e0c70cc
Szymon Bagiński <szymon@wildland.io>
2022-10-13 14:53:25 +0200

    bump rusty bind

--- crates/wildland-cargo-lib/build.rs
+++ crates/wildland-cargo-lib/build.rs
@@ -1,6 +1,6 @@
 #[cfg(feature = "bindings")]
 fn main() -> Result<(), String> {
-    use ffi_macro_build::parse_ffi_module;
+    use rusty_bind_build::parse_ffi_module;
     let ffi_code_target_dir = std::env::var("FFI_CODE_TARGET_DIR")
         .unwrap_or_else(|_| "./_generated_ffi_code/".to_owned());
     parse_ffi_module("src/ffi/mod.rs", &ffi_code_target_dir)?;
