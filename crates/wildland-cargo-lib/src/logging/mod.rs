47b8fcc3baca8c395a0c81d70013997e6a35dcc5
Mateusz Matejuk <esavier@erglabs.org>
2022-10-17 19:29:16 +0200

    fix: [WILX-279] tracing does not log to file * changed to blocking writer that does not need guard to work * changed logger initialization a bit

--- crates/wildland-cargo-lib/src/logging/mod.rs
+++ crates/wildland-cargo-lib/src/logging/mod.rs
@@ -1,28 +1,36 @@
-use std::str::FromStr;
-
+use std::io::{self};
 use tracing::Level;
-use tracing_core::LevelFilter;
 use tracing_subscriber::{
-    fmt, prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt,
+    fmt, fmt::writer::MakeWriterExt, prelude::__tracing_subscriber_SubscriberExt, EnvFilter,
 };
 
 pub(crate) fn init_subscriber(log_level: Level, log_file: Option<String>) -> Result<(), String> {
-    let fmt_layer = fmt::layer().with_target(false).with_level(true);
-    let level_filter =
-        LevelFilter::from_level(Level::from_str(log_level.as_str()).map_err(|e| e.to_string())?);
-
-    let logger = tracing_subscriber::registry()
-        .with(level_filter)
-        .with(fmt_layer);
-    if let Some(log_file) = log_file {
-        let file_appender = tracing_appender::rolling::hourly(".", log_file);
-        let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-
-        logger
-            .with(Some(fmt::Layer::new().with_writer(non_blocking)))
-            .init();
+    if let Some(logfile) = log_file {
+        default_with_file_copy(log_level, logfile);
     } else {
-        logger.init();
-    }
+        default_without_file_copy(log_level);
+    };
+
+    tracing::debug!("logger initialized");
     Ok(())
+}
+
+pub fn default_with_file_copy(log_level: tracing::Level, filename: String) {
+    let file_appender = tracing_appender::rolling::hourly(".", filename);
+    // let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    let subscriber = tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env().add_directive(log_level.into()))
+        .with(fmt::Layer::new().with_writer(file_appender.and(std::io::stderr)));
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Unable to set a global multilogger instance");
+    tracing::debug!("initialized multilogger");
+}
+
+pub fn default_without_file_copy(log_level: tracing::Level) {
+    let subscriber = tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env().add_directive(log_level.into()))
+        .with(fmt::Layer::new().with_writer(io::stdout));
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Unable to set a global stderr-log instance");
+    tracing::debug!("initialized stderr logger");
 }
