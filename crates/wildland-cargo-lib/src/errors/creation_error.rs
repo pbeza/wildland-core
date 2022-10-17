d3af3062af70240deea4d3740801d8567f0f0dde
szymon.baginski <baginski.szymon@gmail.com>
2022-10-17 13:05:33 +0000

    chore [MINOR]: remove unused domain methods of Exception Trait

--- crates/wildland-cargo-lib/src/errors/creation_error.rs
+++ crates/wildland-cargo-lib/src/errors/creation_error.rs
@@ -1,4 +1,4 @@
-use super::{ErrDomain, ExceptionTrait, WildlandXDomain};
+use super::ExceptionTrait;
 use std::fmt::Display;
 
 pub type SingleErrVariantResult<T, E> = Result<T, SingleVariantError<E>>;
@@ -8,16 +8,10 @@ pub enum SingleVariantError<T: Clone> {
     Failure(T),
 }
 
-impl<E: Display + Clone + ErrDomain> ExceptionTrait for SingleVariantError<E> {
+impl<E: Display + Clone> ExceptionTrait for SingleVariantError<E> {
     fn reason(&self) -> String {
         match self {
             Self::Failure(e) => e.to_string(),
-        }
-    }
-
-    fn domain(&self) -> WildlandXDomain {
-        match self {
-            SingleVariantError::Failure(e) => e.domain(),
         }
     }
 }
