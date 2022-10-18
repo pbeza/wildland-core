6901b1bd197de8b8ed7dce92f04c7fe91742e8a8
Mateusz Matejuk <esavier@erglabs.org>
2022-10-18 10:56:33 +0200

    * added temporary switch to handle color (ansi mode) * turned of coloring in file

--- crates/wildland-cargo-lib/src/logging/mod.rs
+++ crates/wildland-cargo-lib/src/logging/mod.rs
@@ -1,35 +1,43 @@
 use std::io::{self};
 use tracing::Level;
-use tracing_subscriber::{
-    fmt, fmt::writer::MakeWriterExt, prelude::__tracing_subscriber_SubscriberExt, EnvFilter,
-};
+use tracing_subscriber::{fmt, prelude::__tracing_subscriber_SubscriberExt, EnvFilter};
 
 pub(crate) fn init_subscriber(log_level: Level, log_file: Option<String>) -> Result<(), String> {
+    let is_ansi = std::env::vars_os().any(|(k, _)| k == "ANSI");
     if let Some(logfile) = log_file {
-        default_with_file_copy(log_level, logfile);
+        default_with_file_copy(log_level, logfile, is_ansi);
     } else {
-        default_without_file_copy(log_level);
+        default_without_file_copy(log_level, is_ansi);
     };
-
     tracing::debug!("logger initialized");
     Ok(())
 }
 
-pub fn default_with_file_copy(log_level: tracing::Level, filename: String) {
+pub fn default_with_file_copy(log_level: tracing::Level, filename: String, is_ansi: bool) {
     let file_appender = tracing_appender::rolling::hourly(".", filename);
     // let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
     let subscriber = tracing_subscriber::registry()
         .with(EnvFilter::from_default_env().add_directive(log_level.into()))
-        .with(fmt::Layer::new().with_writer(file_appender.and(std::io::stderr)));
+        // .with(fmt::Layer::new().with_ansi(is_ansi).with_writer(file_appender.and(std::io::stderr)));
+        .with(
+            fmt::Layer::new()
+                .with_ansi(false)
+                .with_writer(file_appender),
+        )
+        .with(
+            fmt::Layer::new()
+                .with_ansi(is_ansi)
+                .with_writer(std::io::stderr),
+        );
     tracing::subscriber::set_global_default(subscriber)
         .expect("Unable to set a global multilogger instance");
     tracing::debug!("initialized multilogger");
 }
 
-pub fn default_without_file_copy(log_level: tracing::Level) {
+pub fn default_without_file_copy(log_level: tracing::Level, is_ansi: bool) {
     let subscriber = tracing_subscriber::registry()
         .with(EnvFilter::from_default_env().add_directive(log_level.into()))
-        .with(fmt::Layer::new().with_writer(io::stdout));
+        .with(fmt::Layer::new().with_ansi(is_ansi).with_writer(io::stdout));
     tracing::subscriber::set_global_default(subscriber)
         .expect("Unable to set a global stderr-log instance");
     tracing::debug!("initialized stderr logger");
