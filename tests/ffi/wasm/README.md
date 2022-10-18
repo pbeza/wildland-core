8d621e4478784c2933de8acff54f445a9e0ba730
Szymon Bagiński <szymon@wildland.io>
2022-10-18 12:23:02 +0200

    add vectors in WASM

--- tests/ffi/wasm/README.md
+++ tests/ffi/wasm/README.md
@@ -6,6 +6,15 @@ This directory consists of files needed to compile **CargoLib** and run it with 
 
 Run `./build_wasm.sh` command in directory of this *README.md* file. This script will compile **CargoLib** to `wasm32-unknown-emscripten` target. Then compiled library along with generated C++ glue code and handwritten *main.cpp* (includes dependencies) file are used as an input for emscripten in order to generate wasm package and JS glue. The result is two files: `wildland.wasm` and `wildland.js`.
 
+**Requirements**:
+- cargo with `wasm32-unknown-emscripten` target installed
+- available `em++` command
+    example emsdk activation (depends on installation location):
+    ```
+    /./emsdk/emsdk activate latest
+    . /emsdk/emsdk_env.sh
+    ```
+
 ## Example usage of wasm package
 
 Example usage of wasm package is shown in the [`wasm_test.js`](./wasm_test.js) file which can be run with command `node wasm_test.js`.
--- tests/ffi/wasm/wasm_test.js
+++ tests/ffi/wasm/wasm_test.js
@@ -27,10 +27,21 @@ wildland().then((wlib) => {
     var cargo_lib = wlib.create_cargo_lib(lss, cfg);
 
     // Acquire API object for user management
-    var user_api = cargo_lib.user_api()
+    var user_api = cargo_lib.user_api();
 
-    var mnemonic = user_api.generate_mnemonic()
+    var mnemonic = user_api.generate_mnemonic();
     // Rust Strings are not automatically converted to native ones yet
     // so to_string() call is required
-    console.log(mnemonic.get_string().to_string())
+    console.log(mnemonic.get_string().to_string());
+
+    // mnemonic can be restored from vec of words
+    // RustVec<T> can be initialized with constructor named with pattern `RustVec_{T type name}()`
+    var words_vec = new wlib.RustVec_RustString();
+    for (const word of [
+        "enroll", "fat", "stumble", "life", "apology", "rate",
+        "fringe", "mutual", "club", "slam", "ethics", "dinner"]) {
+        words_vec.push(new wlib.String(word));
+    }
+    var restored_mnemonic = user_api.create_mnemonic_from_vec(words_vec);
+    console.log(restored_mnemonic.get_string().to_string())
 });
