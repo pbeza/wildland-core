98c6d170a8e78def8699ab0a53892bea88dfbc59
Ivan Sinitsa <ivan@wildland.io>
2022-12-15 19:53:27 +0100

    WILX-320: Add scripts to build iOS targets

--- ci/build_apple_ios_simulator.sh
+++ ci/build_apple_ios_simulator.sh
@@ -3,9 +3,9 @@
 #     Piotr Isajew (pisajew@wildland.io)
 set -ex
 DEBUG_OPTS="-g"
-BUILD_ROOT=$CI_PROJECT_DIR
-DESTROOT="$BUILD_ROOT/wildlandx_macos.build"
-RUST_SRCDIR=$CI_PROJECT_DIR
+# DESTROOT="$BUILD_ROOT/wildlandx_ios.build"
+DESTROOT="$1"
+RUST_SRCDIR="/Users/wildland/src/wildland-core"
 SWIFT_BRIDGE_OUTDIR="$RUST_SRCDIR/crates/wildland-cargo-lib/_generated_ffi_code"
 INPUT="$SWIFT_BRIDGE_OUTDIR/ffi_swift.swift"
 RUST_LIB="libwildland_cargo_lib.a"
@@ -16,70 +16,16 @@ ARCHS="x86_64 arm64"
 FW_OUT="$DESTROOT/$MODULE.framework"
 PKG_OUT="out_dir"
 
-# Google storage URL for binary SDK uploads.
-UPLOAD_URL="gs://wildland-apple-dev-binaries"
+RUST_ARCH_X86_64=x86_64-apple-ios
+RUST_ARCH_ARM64=aarch64-apple-ios-sim
 
-# URL from which binary packages can be fetched.
-FETCH_URL="https://xcode-proxy.wildland.dev/wildland-apple-dev-binaries"
-
-# GIT repository URL where package manifests should be pushed.
-MANIFEST_REPOSITORY="git@gitlab.com:wildland/corex/sdk-apple.git"
-# Target branch to which package manifests are to be pushed.
-MANIFEST_BRANCH="master"
-
-RUST_ARCH_X86_64=x86_64-apple-darwin
-RUST_ARCH_ARM64=aarch64-apple-darwin
-
-export MACOS_DEPLOYMENT_TARGET=12.3
-export SDKROOT=$(xcrun -sdk macosx${MACOS_DEPLOYMENT_TARGET} --show-sdk-path)
+export IOS_DEPLOYMENT_TARGET=15.5
+export SDKROOT=$(xcrun -sdk iphonesimulator --show-sdk-path)
 
 if [ -d "$DESTROOT" ]; then
     rm -rf "$DESTROOT"
 fi
 
-upload_framework() {
-    local fw="$1"
-    echo uploading "$1"
-
-    MANIFEST_DIR=`mktemp -d`
-    rmdir $MANIFEST_DIR
-    git clone $MANIFEST_REPOSITORY $MANIFEST_DIR
-    SAVED_WD=`pwd`
-    cd $MANIFEST_DIR
-    git checkout $MANIFEST_BRANCH
-    
-    cat > Package.swift <<EOF
-// swift-tools-version: 5.6
-
-import PackageDescription
-
-let package = Package(
-  name: "wildlandx",
-  products: [
-    .library(name: "wildlandx", targets: ["wildlandx"])
-  ],
-  targets: [
-    .binaryTarget(
-      name: "wildlandx",
-      url: "$FETCH_URL/wildlandx.xcframework.zip",
-      checksum: "$(shasum -a 256 $SAVED_WD/wildlandx.xcframework.zip | awk '{print $1}')"
-    ),
-    .testTarget(
-        name: "WildlandXTests",
-        dependencies: ["wildlandx"]
-    )
-  ]
-)
-EOF
-
-    git add Package.swift
-    git commit -m "Build script updated package manifest at $(date +%Y-%m-%d)"
-    git push
-    cd $SAVED_WD
-    
-    gcloud auth activate-service-account --key-file=$CLOUD_CREDENTIALS
-    gsutil cp "$1" $UPLOAD_URL
-}
 mkdir "$DESTROOT"
 mkdir "$FW_OUT"
 
@@ -124,9 +70,9 @@ EOF
 #include "ffi_swift.h"
 #endif
 EOF
-    HEADER_OUT="$FW_OUT/Versions/A/Headers/"
+    HEADER_OUT="$FW_OUT/Headers/"
     test -d "$HEADER_OUT" || mkdir -p "$HEADER_OUT"
-    MOD_OUT="$FW_OUT/Versions/A/Modules/$MODULE.swiftmodule/"
+    MOD_OUT="$FW_OUT/Modules/$MODULE.swiftmodule/"
     mkdir -p $MOD_OUT
  
 
@@ -134,7 +80,7 @@ cat > "$HEADER_OUT/$MODULE.h" <<EOF
 #import <Foundation/Foundation.h>
 EOF
 cp $GLUE_MOD/*.h $HEADER_OUT
-cat > "$FW_OUT/Versions/A/Modules/module.modulemap" <<EOF
+cat > "$FW_OUT/Modules/module.modulemap" <<EOF
 
 framework module $MODULE {
   umbrella header "$MODULE.h"
@@ -149,17 +95,19 @@ framework module $MODULE {
   }
 }
 EOF
-cp $FW_OUT/Versions/A/Modules/module.modulemap .
+cp $FW_OUT/Modules/module.modulemap .
 ln -s $HEADER_OUT .
 INPUT=input.swift
     cat $SWIFT_BRIDGE_OUTDIR/ffi_swift.swift >> $INPUT
+    
+    ARCH_TARGET=$arch-apple-ios$IOS_DEPLOYMENT_TARGET-simulator
     
     # Prepare module metadata
     xcrun swiftc -v -module-name "$MODULE" \
           -I . \
           $INPUT \
           -Rmodule-loading \
-          -target $arch-apple-macos12.3 \
+          -target $ARCH_TARGET \
           $DEBUG_OPTS \
           -parse-as-library \
           -emit-dependencies \
@@ -178,7 +126,7 @@ INPUT=input.swift
           -I . \
           -module-name "$MODULE" \
           -stack-check \
-          -target $arch-apple-macos12.3 \
+          -target $ARCH_TARGET \
           $DEBUG_OPTS \
           -parse-as-library \
           -import-underlying-module \
@@ -188,21 +136,21 @@ INPUT=input.swift
     # Link architecture-specific binary
     xcrun clang \
           -v \
-          -target $arch-apple-macos12.3 \
+          -target $ARCH_TARGET \
           -dynamiclib \
           $OBJ_OUT \
-          -install_name @rpath/$MODULE.framework/Versions/A/$MODULE \
+          -install_name @rpath/$MODULE.framework/$MODULE \
           -L. \
-          -L/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx -L/usr/lib/swift \
+          -L/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphonesimulator -L/usr/lib/swift \
           -lwildland_cargo_lib \
           -o $DESTDIR/$MODULE
-    cp -v $MODULE_PATH $MOD_OUT/$arch-apple-macos.swiftmodule
-    cp -v $MODULE_INTERFACE_PATH $MOD_OUT/$arch-apple-macos.swiftinterface
+    cp -v $MODULE_PATH $MOD_OUT/$arch-apple-ios-simulator.swiftmodule
+    cp -v $MODULE_INTERFACE_PATH $MOD_OUT/$arch-apple-ios-simulator.swiftinterface
     cp -v $DESTDIR/$MODULE-Swift.h $HEADER_OUT
 done
 
-xcrun lipo -create $DESTROOT/*/$MODULE -output $FW_OUT/Versions/A/$MODULE
-RES_OUT="$FW_OUT/Versions/A/Resources/"
+xcrun lipo -create $DESTROOT/*/$MODULE -output $FW_OUT/$MODULE
+RES_OUT="$FW_OUT/"
 test -d "$RES_OUT" || mkdir -p "$RES_OUT"
 
 cat > "$RES_OUT/Info.plist" <<EOF
@@ -224,36 +172,14 @@ cat > "$RES_OUT/Info.plist" <<EOF
         <string>1.0</string>
         <key>CFBundleSupportedPlatforms</key>
         <array>
-           <string>MacOSX</string>
+           <string>iPhoneSimulator</string>
         </array>
         <key>CFBundleVersion</key>
         <string>1</string>
         <key>LSMinimumSystemVersion</key>
-        <string>12.3</string>
+        <string>$IOS_DEPLOYMENT_TARGET</string>
         <key>NSHumanReadableCopyright</key>
         <string>Copyright (C) 2022, Golem Foundation</string>
 </dict>
 </plist>
 EOF
-
-cd $FW_OUT/Versions
-ln -s A Current
-cd ..
-ln -s Versions/Current/Headers .
-ln -s Versions/Current/Modules .
-ln -s Versions/Current/Resources .
-
-ln -s Versions/Current/$MODULE .
-
-# TODO: Create frameworks for different platforms
-cd $DESTROOT
-xcodebuild -create-xcframework \
-           -framework $FW_OUT \
-           -output wildlandx.xcframework
-mkdir $PKG_OUT
-ditto -c -k --sequesterRsrc --keepParent wildlandx.xcframework $PKG_OUT/wildlandx.xcframework.zip
-cd $PKG_OUT
-
-if [ "$CI_COMMIT_BRANCH" = "master" ] || [ "$CI_COMMIT_BRANCH" = "develop" ]; then
-    upload_framework wildlandx.xcframework.zip
-fi
---
+++ ci/build_apple_universal.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+# Maintainers: 
+#     Piotr Isajew (pisajew@wildland.io)
+#     Ivan Sinitsa (ivan@wildland.io)
+
+set -ex
+# Google storage URL for binary SDK uploads.
+UPLOAD_URL="gs://wildland-apple-dev-binaries"
+
+# URL from which binary packages can be fetched.
+FETCH_URL="https://xcode-proxy.wildland.dev/wildland-apple-dev-binaries"
+
+# GIT repository URL where package manifests should be pushed.
+MANIFEST_REPOSITORY="git@gitlab.com:wildland/corex/sdk-apple.git"
+# Target branch to which package manifests are to be pushed.
+MANIFEST_BRANCH="master"
+
+# Define 
+BUILD_ROOT="/Users/wildland/src/wildland-core"
+MODULE="wildlandx"
+PKG_OUT="out_dir"
+
+# Build iOS framework
+DESTROOT="$BUILD_ROOT/wildlandx_ios.build"
+FW_IOS_OUT="$DESTROOT/$MODULE.framework"
+./ci/build_apple_ios.sh $DESTROOT
+
+# Build iOS Simulator framework
+DESTROOT="$BUILD_ROOT/wildlandx_ios_simulator.build"
+FW_IOS_SIM_OUT="$DESTROOT/$MODULE.framework"
+./ci/build_apple_ios_simulator.sh $DESTROOT
+
+# Build macOS framework
+DESTROOT="$BUILD_ROOT/wildlandx_mac.build"
+FW_MAC_OUT="$DESTROOT/$MODULE.framework"
+./ci/build_apple_mac.sh $DESTROOT
+
+# Create output folder
+FW_UNIVERSAL_OUT="wildlandx_universal.build"
+mkdir $FW_UNIVERSAL_OUT
+
+# Create universal framework for all the previous jobs
+cd $FW_UNIVERSAL_OUT
+xcodebuild -create-xcframework \
+           -framework $FW_IOS_OUT \
+           -framework $FW_IOS_SIM_OUT \
+           -framework $FW_MAC_OUT \
+           -output "wildlandx.xcframework"
+mkdir $PKG_OUT
+ditto -c -k --sequesterRsrc --keepParent wildlandx.xcframework $PKG_OUT/wildlandx.xcframework.zip
+cd $PKG_OUT
+
+# Perform framework upload
+upload_framework() {
+    local fw="$1"
+    echo uploading "$1"
+
+    MANIFEST_DIR=`mktemp -d`
+    rmdir $MANIFEST_DIR
+    git clone $MANIFEST_REPOSITORY $MANIFEST_DIR
+    SAVED_WD=`pwd`
+    cd $MANIFEST_DIR
+    git checkout $MANIFEST_BRANCH
+    
+    cat > Package.swift <<EOF
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+  name: "wildlandx",
+  platforms: [
+    .macOS(.v12), .iOS(.v15)
+  ],
+  products: [
+    .library(name: "wildlandx", targets: ["wildlandx"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "wildlandx",
+      url: "$FETCH_URL/wildlandx.xcframework.zip",
+      checksum: "$(shasum -a 256 $SAVED_WD/wildlandx.xcframework.zip | awk '{print $1}')"
+    ),
+    .testTarget(
+        name: "WildlandXTests",
+        dependencies: ["wildlandx"]
+    )
+  ]
+)
+EOF
+
+    git add Package.swift
+    git commit -m "Build script updated package manifest at $(date +%Y-%m-%d)"
+    git push
+    cd $SAVED_WD
+    
+    gcloud auth activate-service-account --key-file=$CLOUD_CREDENTIALS
+    gsutil cp "$1" $UPLOAD_URL
+}
+
+# Upload the framework
+if [ "$CI_COMMIT_BRANCH" = "master" ] || [ "$CI_COMMIT_BRANCH" = "develop" ]; then
+    upload_framework wildlandx.xcframework.zip
+fi
+
+if [ -d "$FW_UNIVERSAL_OUT" ]; then
+    rm -rf "$FW_UNIVERSAL_OUT"
+fi
