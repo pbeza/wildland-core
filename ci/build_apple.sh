cec55e5417fa9acc4e5d0106390ccdcc13961524
Ivan Sinitsa <ivan@wildland.io>
2022-12-20 18:51:53 +0100

    WILX-320: Update apple build script, add SDK upload script

--- ci/build_apple.sh
+++ ci/build_apple.sh
@@ -4,19 +4,10 @@
 #     Ivan Sinitsa (ivan@wildland.io)
 
 set -ex
-# Google storage URL for binary SDK uploads.
-UPLOAD_URL="gs://wildland-apple-dev-binaries"
-
-# URL from which binary packages can be fetched.
-FETCH_URL="https://xcode-proxy.wildland.dev/wildland-apple-dev-binaries"
-
-# GIT repository URL where package manifests should be pushed.
-MANIFEST_REPOSITORY="git@gitlab.com:wildland/corex/sdk-apple.git"
-# Target branch to which package manifests are to be pushed.
-MANIFEST_BRANCH="master"
 
 # Define
-BUILD_ROOT=$CI_PROJECT_DIR
+# BUILD_ROOT=$CI_PROJECT_DIR
+BUILD_ROOT="/Users/wildland/src/wildland-core"
 MODULE="wildlandx"
 PKG_OUT="out_dir"
 
@@ -53,57 +44,3 @@ xcodebuild -create-xcframework \
            -output "wildlandx.xcframework"
 mkdir $PKG_OUT
 ditto -c -k --sequesterRsrc --keepParent wildlandx.xcframework $PKG_OUT/wildlandx.xcframework.zip
-cd $PKG_OUT
-
-# Perform framework upload
-upload_framework() {
-    local fw="$1"
-    echo uploading "$1"
-
-    MANIFEST_DIR=`mktemp -d`
-    rmdir $MANIFEST_DIR
-    git clone $MANIFEST_REPOSITORY $MANIFEST_DIR
-    SAVED_WD=`pwd`
-    cd $MANIFEST_DIR
-    git checkout $MANIFEST_BRANCH
-    
-    cat > Package.swift <<EOF
-// swift-tools-version: 5.6
-
-import PackageDescription
-
-let package = Package(
-    name: "wildlandx",
-    platforms: [
-        .macOS(.v12), .iOS(.v15)
-    ],
-    products: [
-        .library(name: "wildlandx", targets: ["wildlandx"])
-    ],
-    targets: [
-        .binaryTarget(
-            name: "wildlandx",
-            url: "$FETCH_URL/wildlandx.xcframework.zip",
-            checksum: "$(shasum -a 256 $SAVED_WD/wildlandx.xcframework.zip | awk '{print $1}')"
-        ),
-        .testTarget(
-            name: "WildlandXTests",
-            dependencies: ["wildlandx"]
-        )
-    ]
-)
-EOF
-
-    git add Package.swift
-    git commit -m "Build script updated package manifest at $(date +%Y-%m-%d)"
-    git push
-    cd $SAVED_WD
-
-    gcloud auth activate-service-account --key-file=$CLOUD_CREDENTIALS
-    gsutil cp "$1" $UPLOAD_URL
-}
-
-# Upload the framework
-if [ "$CI_COMMIT_BRANCH" = "master" ] || [ "$CI_COMMIT_BRANCH" = "develop" ]; then
-    upload_framework wildlandx.xcframework.zip
-fi
