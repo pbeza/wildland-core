4a2ad8a059cbcfb4bd015c8f25468da2b3e201de
ihor rudynskyi <ihor.rudynskyi@gmail.com>
2022-12-27 12:23:48 +0100

    tmp fix

--- crates/wildland-cargo-lib/build.rs
+++ crates/wildland-cargo-lib/build.rs
@@ -20,6 +20,7 @@ fn main() -> anyhow::Result<()> {
     use rusty_bind_build::parse_ffi_module;
     let ffi_code_target_dir = std::env::var("FFI_CODE_TARGET_DIR")
         .unwrap_or_else(|_| "./_generated_ffi_code/".to_owned());
+    std::env::set_var("CARGO_CFG_TARGET_FEATURE", "");
     parse_ffi_module("src/ffi/mod.rs", &ffi_code_target_dir)?;
     println!("cargo:rerun-if-changed=src/ffi/mod.rs");
     Ok(())
