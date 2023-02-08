909c84b8d25c94408f90368cc1f980cbe28d2606
ihor rudynskyi <ihor.rudynskyi@gmail.com>
2023-02-08 12:07:24 +0100

    refactor write method

--- crates/wildland-dfs/src/storage_backends/s3/client.rs
+++ crates/wildland-dfs/src/storage_backends/s3/client.rs
@@ -62,6 +62,43 @@ impl WildlandS3Client {
             client: build_s3_client(credentials, region),
         }
     }
+
+    #[allow(clippy::too_many_arguments)]
+    fn multipart_copy(
+        &self,
+        begin: usize,
+        end: usize,
+        bucket_name: &str,
+        copy_source: &str,
+        etag: &Option<String>,
+        key: &Option<String>,
+        upload_id: &Option<String>,
+        part_number: &mut i32,
+    ) -> Result<Vec<CompletedPart>, S3Error> {
+        let mut completed_parts = vec![];
+        execute_by_step(begin, end, MAXIMUM_PART_SIZE, |begin, end| {
+            let UploadPartCopyOutput {
+                copy_part_result, ..
+            } = self.rt.block_on(async {
+                self.client
+                    .upload_part_copy()
+                    .bucket(bucket_name)
+                    .copy_source(copy_source)
+                    .set_copy_source_if_match(etag.clone())
+                    .copy_source_range(format!("bytes={begin}-{}", end - 1))
+                    .set_key(key.clone())
+                    .part_number(*part_number)
+                    .set_upload_id(upload_id.clone())
+                    .send()
+                    .await
+            })?;
+
+            completed_parts.push(to_completed_part(copy_part_result, *part_number));
+            *part_number += 1;
+            Ok(())
+        })?;
+        Ok(completed_parts)
+    }
 }
 
 impl S3Client for WildlandS3Client {
@@ -168,31 +205,6 @@ impl S3Client for WildlandS3Client {
         file_size: usize,
         etag: Option<String>,
     ) -> Result<WriteResp, S3Error> {
-        let mut bytes = vec![];
-
-        if position != 0 && position < MINIMUM_PART_SIZE {
-            // S3 doesn't allow copying chunks smaller then MINIMUM_PART_SIZE so we need to copy those bytes manually
-            bytes = self.read_object(path, bucket_name, 0, position, etag.clone())?;
-            position = 0;
-        }
-
-        bytes.extend_from_slice(buf);
-
-        let position_after_write = position + bytes.len();
-        if bytes.len() < MINIMUM_PART_SIZE && (position_after_write < file_size) {
-            // Only the last chunk can be smaller then MINIMUM_PART_SIZE
-            // so we need to copy some data from the original file
-            let remaining_bytes = self.read_object(
-                path,
-                bucket_name,
-                position + bytes.len(),
-                MINIMUM_PART_SIZE - bytes.len(),
-                etag.clone(),
-            )?;
-
-            bytes.extend(remaining_bytes.into_iter())
-        }
-
         let CreateMultipartUploadOutput { upload_id, key, .. } = self.rt.block_on(async {
             self.client
                 .create_multipart_upload()
@@ -202,7 +214,7 @@ impl S3Client for WildlandS3Client {
                 .await
         })?;
 
-        let guard = scopeguard::guard((), |_| {
+        let aboart_upload_on_exit = scopeguard::guard((), |_| {
             tracing::error!("Failed to perform multipart upload. Aborting.");
             if self
                 .rt
@@ -221,93 +233,98 @@ impl S3Client for WildlandS3Client {
             }
         });
 
+        let mut buffer = vec![];
         let mut part_number = 1;
         let mut completed_parts = vec![];
         let copy_source = format!("{bucket_name}/{}", path.to_string_lossy());
 
-        if position != 0 {
-            execute_by_step(0, position, MAXIMUM_PART_SIZE, |begin, end| {
-                let UploadPartCopyOutput {
-                    copy_part_result, ..
-                } = self.rt.block_on(async {
-                    self.client
-                        .upload_part_copy()
-                        .bucket(bucket_name)
-                        .copy_source(&copy_source)
-                        .set_copy_source_if_match(etag.clone())
-                        .copy_source_range(format!("bytes={begin}-{}", end - 1))
-                        .set_key(key.clone())
-                        .part_number(part_number)
-                        .set_upload_id(upload_id.clone())
-                        .send()
-                        .await
-                })?;
-
-                completed_parts.push(to_completed_part(copy_part_result, part_number));
-                part_number += 1;
-                Ok(())
-            })?;
+        // Copy range [0, position) from the original file
+        match position {
+            // Nothing to copy
+            0 => (),
+            // Not enough bytes to use multipart copy. We need to copy it manually
+            p if p < MINIMUM_PART_SIZE => {
+                buffer = self.read_object(path, bucket_name, 0, position, etag.clone())?;
+                position = 0;
+            }
+            _ => {
+                let parts = self.multipart_copy(
+                    0,
+                    position,
+                    bucket_name,
+                    &copy_source,
+                    &etag,
+                    &key,
+                    &upload_id,
+                    &mut part_number,
+                )?;
+                completed_parts.extend(parts);
+            }
         }
 
-        let position_after_write = position + bytes.len();
+        buffer.extend_from_slice(buf);
+        let mut position_after_write = position + buffer.len();
 
-        let UploadPartOutput {
-            e_tag,
-            checksum_crc32,
-            checksum_crc32_c,
-            checksum_sha1,
-            checksum_sha256,
-            ..
-        } = self.rt.block_on(async {
-            self.client
-                .upload_part()
-                .body(bytes.into())
-                .bucket(bucket_name)
-                .set_key(key.clone())
-                .part_number(part_number)
-                .set_upload_id(upload_id.clone())
-                .send()
-                .await
+        // We might not have enough bytes to perform multipart upload so wee need to take some from the original file.
+        // Buffer size may not reach MINIMUM_PART_SIZE after that "if" statement but it not a problem.
+        // It means that we are uploading the last part of a file and MINIMUM_PART_SIZE restriction doesn't need to be fullfiled.
+        if buffer.len() < MINIMUM_PART_SIZE && position_after_write < file_size {
+            let remaining_bytes = self.read_object(
+                path,
+                bucket_name,
+                position + buffer.len(),
+                MINIMUM_PART_SIZE - buffer.len(),
+                etag.clone(),
+            )?;
+            buffer.extend(remaining_bytes);
+        }
+
+        buffer.chunks(MINIMUM_PART_SIZE).try_for_each(|chunk| {
+            let UploadPartOutput {
+                e_tag,
+                checksum_crc32,
+                checksum_crc32_c,
+                checksum_sha1,
+                checksum_sha256,
+                ..
+            } = self.rt.block_on(async {
+                self.client
+                    .upload_part()
+                    .body(chunk.to_vec().into())
+                    .bucket(bucket_name)
+                    .set_key(key.clone())
+                    .part_number(part_number)
+                    .set_upload_id(upload_id.clone())
+                    .send()
+                    .await
+            })?;
+            completed_parts.push(
+                CompletedPart::builder()
+                    .set_e_tag(e_tag)
+                    .set_checksum_crc32(checksum_crc32)
+                    .set_checksum_crc32_c(checksum_crc32_c)
+                    .set_checksum_sha1(checksum_sha1)
+                    .set_checksum_sha256(checksum_sha256)
+                    .part_number(part_number)
+                    .build(),
+            );
+            part_number += 1;
+            Ok::<_, S3Error>(())
         })?;
-        completed_parts.push(
-            CompletedPart::builder()
-                .set_e_tag(e_tag)
-                .set_checksum_crc32(checksum_crc32)
-                .set_checksum_crc32_c(checksum_crc32_c)
-                .set_checksum_sha1(checksum_sha1)
-                .set_checksum_sha256(checksum_sha256)
-                .part_number(part_number)
-                .build(),
-        );
-        part_number += 1;
 
+        position_after_write = position + buffer.len();
         if position_after_write < file_size {
-            execute_by_step(
+            let parts = self.multipart_copy(
                 position_after_write,
                 file_size,
-                MAXIMUM_PART_SIZE,
-                |begin, end| {
-                    let UploadPartCopyOutput {
-                        copy_part_result, ..
-                    } = self.rt.block_on(async {
-                        self.client
-                            .upload_part_copy()
-                            .bucket(bucket_name)
-                            .copy_source(copy_source.clone())
-                            .set_copy_source_if_match(etag.clone())
-                            .copy_source_range(format!("bytes={begin}-{}", end - 1))
-                            .set_key(key.clone())
-                            .part_number(part_number)
-                            .set_upload_id(upload_id.clone())
-                            .send()
-                            .await
-                    })?;
-
-                    completed_parts.push(to_completed_part(copy_part_result, part_number));
-                    part_number += 1;
-                    Ok(())
-                },
+                bucket_name,
+                &copy_source,
+                &etag,
+                &key,
+                &upload_id,
+                &mut part_number,
             )?;
+            completed_parts.extend(parts);
         }
 
         let CompleteMultipartUploadOutput { e_tag, .. } = self.rt.block_on(async {
@@ -325,7 +342,7 @@ impl S3Client for WildlandS3Client {
                 .await
         })?;
 
-        scopeguard::ScopeGuard::into_inner(guard); // defuse
+        scopeguard::ScopeGuard::into_inner(aboart_upload_on_exit); // defuse
 
         Ok(WriteResp {
             bytes_count: buf.len(),
